a08ff0767fc30fc2f96aa09eeaaf8c5b8f355113
icet-sharadha <icet.sharadha@gmail.com>
2024-07-28 17:08:57 +0530

    get text

--- src/controller/SampleController.java
+++ src/controller/SampleController.java
@@ -2,12 +2,17 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SampleController {
     public Label lblSample;
+    public TextField txtUserName;
+    public TextField txtPassword;
 
     public void btnClickMeOnAction(ActionEvent actionEvent) {
-        lblSample.setText("Hello Java-FX");
+        String userName = txtUserName.getText();
 
+        String password = txtPassword.getText();
+        System.out.println("User Name : "+userName+"\n"+"Password : "+password);
     }
 }
